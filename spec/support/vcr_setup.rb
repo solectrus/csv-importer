53b5da2388a7ddb8154d56cd8bcddbf85c6f18a4
Georg Ledermann <georg@ledermann.dev>
2024-01-25 08:40:57 +0100

    VCR cleanup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -15,4 +15,10 @@ VCR.configure do |config|
   end
 
   config.configure_rspec_metadata!
+
+  record_mode = ENV['VCR'] ? ENV['VCR'].to_sym : :once
+  config.default_cassette_options = {
+    record: record_mode,
+    allow_playback_repeats: true,
+  }
 end
