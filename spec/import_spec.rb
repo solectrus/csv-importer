34f5f27c110ce6c664520099ab5807e620f9f3c9
Georg Ledermann <georg@ledermann.dev>
2024-01-25 07:03:10 +0100

    Test coverage 100%

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -1,5 +1,5 @@
 describe Import do
-  let(:config) { Config.from_env }
+  let(:config) { Config.from_env(import_pause: 0.01) }
 
   describe '#run', vcr: { cassette_name: 'import' } do
     subject(:run) { described_class.run(config:) }
