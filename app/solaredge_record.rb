b6ea9b98e94d319192cea87e4658ab47371d4af1
Sascha Böck <47642539+AlpenFlizzer@users.noreply.github.com>
2024-06-01 15:20:52 +0200

    Update solaredge_record.rb
    added comments for further explanation

--- app/solaredge_record.rb
+++ app/solaredge_record.rb
@@ -1,3 +1,13 @@
+###--------------------------------------------------------------------------------------------------------------------------------------------###
+### Expected column header: Time,Energie (Wh),ZählerBezugs-Zähler E (Wh),ZählerEinspeise-Zähler E (Wh)                                         ###
+### Files have to be exported from https://monitoring.solaredge.com/solaredge-web/p/site/<YOURSITE>/#/charts.                                  ###
+### On the left, select "Auswerten" an then add all datasources, that shall be exported.                                                       ###
+### In this case select: <YOUR_SOLAR-PLANT_NAME>/Energie, Zähler/Bezugs-Zähler/Energie and Zähler/Einspeise-Zähler/Energie                     ###
+### In the top right corner of the chartr window, there is the button for csv export.                                                          ###
+### Attention: You might need to ask your installer/solarplant-admin to give you the necessary privileges to do exports.                       ###
+### During export, select the suitable time window and choose "Täglich" for "Auflösung" (different timespans allow different max. resolutions) ###
+###--------------------------------------------------------------------------------------------------------------------------------------------###
+
 require_relative 'base_record'
 
 class SolaredgeRecord < BaseRecord
@@ -16,13 +26,11 @@ class SolaredgeRecord < BaseRecord
       inverter_power:,
       grid_power_plus:,
       grid_power_minus:,
-      # There is no data for the wallbox, but we can estimate it
+      # There is no data for house_power, it has to be calculated
       house_power: calculated_house_power,
     }
   end
-
-  #Time,Energie (Wh),ZählerBezugs-Zähler E (Wh),ZählerEinspeise-Zähler E (Wh)
-
+  
   def inverter_power
     @inverter_power ||= parse_kw(row, 'Energie (Wh)')
   end
